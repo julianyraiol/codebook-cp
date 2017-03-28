5abecde52489bbdcc32fab134cf701c6f4ed35cd
Juliany Raiol <julianyraiol@users.noreply.github.com>
2017-03-28 12:34:28 -0400

    Crivo de eratóstenes

---
+++ Teoria dos Números/crivo.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(false);
+#define MAXN 50001
+
+using namespace std;
+
+typedef  vector<int> vi;
+
+vi primos;
+
+bitset<MAXN> ehprimo;
+
+void crivo(int n){
+	
+	ehprimo.set();
+	
+	ehprimo[0] = ehprimo[1] = 0;
+	for(int i = 2; i <= n; i++)
+		if(ehprimo[i])
+			for(int j = i+i; j < n; j+=i)
+				ehprimo[j] = 0;
+	
+	primos.push_back(2);
+	for(int i = 3; i <= n; i++)
+		if(ehprimo[i])
+			primos.push_back(i);
+}
+
+int main(){_
+	crivo(MAXN);
+	for(int i = 2; i < primos.size(); i++)
+		cout << primos[i] << " ";
+	cout << endl;
+	return 0;
+}
