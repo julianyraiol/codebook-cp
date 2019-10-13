d3c949099c0a916272655ab0521f0545f24d090a
julianyraiol <juliany@motorola.com>
2019-10-13 11:29:43 -0400

    add day10.cpp
    * bit magic

---
+++ Hacker_Rank/30_days_of_code/day10.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n, count=0;
+    cin >> n;
+
+    while(n){
+      n = n & (n << 1);
+      count++;
+    }
+    
+    cout << count << endl;
+    return 0;
+}
