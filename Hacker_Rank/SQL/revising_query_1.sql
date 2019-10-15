6b4401b82fd2b8d633bae16282e619c1f1a2af5e
julianyraiol <juliany@motorola.com>
2019-10-15 17:33:11 -0400

    SQL Questions - Revising select query II

--- Hacker_Rank/SQL/revising_query_1.sql
+++ Hacker_Rank/SQL/revising_query_1.sql
@@ -1,4 +1,8 @@
-#Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+# Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
 
 
 SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA'
+
+# Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
+
+SELECT NAME FROM CITY WHERE POPULATION >= 120000 AND COUNTRYCODE = 'USA'
