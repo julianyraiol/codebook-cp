44a0c9c35f53b66b0cbd646fb0d8e88d42f5aab0
julianyraiol <juliany@motorola.com>
2019-10-16 10:35:12 -0400

    SQL - Select all columns

--- Hacker_Rank/SQL/revising_query_1.sql
+++ Hacker_Rank/SQL/revising_query_1.sql
@@ -6,3 +6,7 @@ SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA'
 # Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
 
 SELECT NAME FROM CITY WHERE POPULATION >= 120000 AND COUNTRYCODE = 'USA'
+
+# Query all columns (attributes) for every row in the CITY table.
+
+SELECT * FROM CITY
