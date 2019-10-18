d4fd2f2914a0bd824ec7604bcc18fe491ce52651
julianyraiol <juliany@motorola.com>
2019-10-18 14:41:17 -0400

    SQL Questions - Add group by in query

--- Hacker_Rank/SQL/basic_join.sql
+++ Hacker_Rank/SQL/basic_join.sql
@@ -6,3 +6,23 @@ FROM CITY
 INNER JOIN COUNTRY
 ON CITY.COUNTRYCODE = COUNTRY.CODE
 WHERE COUNTRY.CONTINENT = 'Asia'
+
+/*Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns. */
+
+SELECT CITY.NAME
+FROM CITY
+INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = 'Africa' 
+
+/*Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) 
+and their respective average city 
+populations (CITY.Population) rounded down to the nearest integer. 
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
+
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION))
+FROM COUNTRY
+INNER JOIN CITY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT
